ad268a9faa558c5ffc5f86926051c92600f26ee0
Andrey Tedeev <andrey.tedeev@outlook.com>
2021-12-16 22:09:50 +0700

    add : ApiController v1 add : pom properties for encoding

---
+++ app/controllers/v1/ApiController.java
@@ -0,0 +1,17 @@
+package controllers.v1;
+
+import play.mvc.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class ApiController extends Controller {
+
+    public Result users() {
+        return ok();
+    }
+
+    
+
+}
